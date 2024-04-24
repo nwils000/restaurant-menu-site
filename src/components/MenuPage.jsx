638fccac79bc7eee91fc14b5ee13cb7facb385b7
nwils000 <nwils000@gmail.com>
2024-04-24 14:23:02 -0400

    got the food properly stored in state

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -1,14 +1,32 @@
 import Navbar from './Navbar';
 import FoodSection from './FoodSection';
 import Footer from './Footer';
+import axios from 'axios';
 import '../styles/MenuPage.css';
+import { useEffect, useState } from 'react';
 
 export default function MenuPage() {
+  const [foodData, setFoodData] = useState('foooood');
+
+  useEffect(() => {
+    async function fetchFoodData() {
+      try {
+        const res = await axios.get('https://www.jsonkeeper.com/b/MDXW');
+        setFoodData(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchFoodData();
+  }, []);
   return (
     <div>
       <Navbar />
       <main className="menu-page">
-        <FoodSection />
+        <div>{foodData[0].title}</div>
+        <FoodSection foodData={foodData} />
         <FoodSection />
         <FoodSection />
       </main>
