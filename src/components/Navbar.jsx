271a857503e404b170226b83e7668d2eaffd3d2a
nwils000 <nwils000@gmail.com>
2024-04-24 13:42:14 -0400

    added the navbar and basic styles to it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <Link to="/" className="logo-link">
+        <img src="/assets/BIRDS.png" alt="birds" />
+      </Link>
+      <div className="nav-links">
+        <Link className="nav-link" to="/menu">
+          Menu
+        </Link>
+        <Link className="nav-link" to="/menu">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
 }
