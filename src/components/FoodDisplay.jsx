4ecb1ff6cdb59cbc104db885f015b37ffa5ddb32
nwils000 <nwils000@gmail.com>
2024-04-24 11:34:07 -0400

    got the basic styles of the food displays done

--- src/components/FoodDisplay.jsx
+++ src/components/FoodDisplay.jsx
@@ -1,7 +1,18 @@
+import Likes from './Likes';
+
 export default function FoodDisplay() {
   return (
-    <section>
+    <section className="food-display">
       <img src="/assets/BIRDS.png" alt="birds" className="food-display-image" />
+      <div>
+        <h3>Breakfast Sampler</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius odio
+          impedit perspiciatis quisquam repellat in at, iste consequuntur,
+          pariatur repellendus nostrum?
+        </p>
+      </div>
+      <Likes />
     </section>
   );
 }
